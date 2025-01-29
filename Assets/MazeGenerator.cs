c10195c852c1b5bc0b0c72e02b8419789d6acce0
AKuc-96 <anna.kuc.9603@gmail.com>
2025-01-29 01:05:22 +0100

    Changed maze generation logic

--- Assets/MazeGenerator.cs
+++ Assets/MazeGenerator.cs
@@ -18,10 +18,30 @@ public class MazeGenerator : MonoBehaviour
 
     void MakeSomeRandomWalls()
     {
-        int randomNumberOfCells = Random.Range(1, 20);
-        for (int i = 0; i < randomNumberOfCells; i++)
+        Vector2 mazeCellPos = new Vector2 (Random.Range(-8, -1), Random.Range(1, 4));
+        Vector2 scaleChange = new Vector2 (Random.Range(0, 6), Random.Range(0, 3));
+
+        GameObject prefabInst = Instantiate(_mazeCellPrefab, mazeCellPos, Quaternion.identity);
+
+        Vector2 cellScale = transform.localScale;
+        
+        if (scaleChange.x == 0 || scaleChange.y == 0)
         {
-            Instantiate(_mazeCellPrefab, new Vector2(Random.Range(-8, 8), Random.Range(-4, 4)), Quaternion.identity);
+            prefabInst.transform.localScale = (Vector2)prefabInst.transform.localScale + (Vector2)scaleChange;
         }
+
+        Instantiate(prefabInst, new Vector2(-mazeCellPos.x, mazeCellPos.y), Quaternion.identity);
+        Instantiate(prefabInst, new Vector2(mazeCellPos.x, -mazeCellPos.y), Quaternion.identity);
+        Instantiate(prefabInst, new Vector2(-mazeCellPos.x, -mazeCellPos.y), Quaternion.identity);
+        
+        //some useful code, please don't delete it
+        // for (int i = 0; i < randomNumberOfCells; i++)
+        // {
+        //     lastPosition = prefabInst.transform.position;
+        //     if (Vector2.Distance(prefabInst.transform.position, lastPosition) == 1)
+        //     {
+        //         prefabInst.transform.position = (Vector2)prefabInst.transform.position + new Vector2(1, 1); 
+        //     }
+        // }
     }
 }
