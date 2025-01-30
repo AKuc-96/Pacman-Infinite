b636afa6aeef182ea8dc3d63e491fe46e4d7b9af
AKuc-96 <anna.kuc.9603@gmail.com>
2025-01-30 01:39:36 +0100

    Added more random walls

--- Assets/MazeGenerator.cs
+++ Assets/MazeGenerator.cs
@@ -18,21 +18,36 @@ public class MazeGenerator : MonoBehaviour
 
     void MakeSomeRandomWalls()
     {
-        Vector2 mazeCellPos = new Vector2 (Random.Range(-8, -1), Random.Range(1, 4));
-        Vector2 scaleChange = new Vector2 (Random.Range(0, 6), Random.Range(0, 3));
-
-        GameObject prefabInst = Instantiate(_mazeCellPrefab, mazeCellPos, Quaternion.identity);
-
+        Vector2 mazeCellPos = new Vector2 (Random.Range(-7, -1), Random.Range(1, 4));
+        Vector2 scaleChange = new Vector2 (Random.Range(0, 3), Random.Range(0, 3));
         Vector2 cellScale = transform.localScale;
-        
-        if (scaleChange.x == 0 || scaleChange.y == 0)
+        int randomCount = Random.Range(2, 5);
+
+        for (int i = 0; i < randomCount; i++)
         {
-            prefabInst.transform.localScale = (Vector2)prefabInst.transform.localScale + (Vector2)scaleChange;
+            GameObject prefabInst = Instantiate(_mazeCellPrefab, mazeCellPos, Quaternion.identity);
+            
+            if (scaleChange.x == 0 || scaleChange.y == 0)
+            {
+                prefabInst.transform.localScale = (Vector2)prefabInst.transform.localScale + (Vector2)scaleChange;
+            }
+
+            Instantiate(prefabInst, new Vector2(-mazeCellPos.x, mazeCellPos.y), Quaternion.identity);
+            Instantiate(prefabInst, new Vector2(mazeCellPos.x, -mazeCellPos.y), Quaternion.identity);
+            Instantiate(prefabInst, new Vector2(-mazeCellPos.x, -mazeCellPos.y), Quaternion.identity);
+
+            mazeCellPos = (Vector2)mazeCellPos + new Vector2(2, 2);
+            if (mazeCellPos.y > 4)
+            {
+                mazeCellPos = (Vector2)mazeCellPos + new Vector2(0, -4);
+            }
+            if (mazeCellPos.x < -7)
+            {
+                mazeCellPos = (Vector2)mazeCellPos + new Vector2(1, 0);
+            }
         }
 
-        Instantiate(prefabInst, new Vector2(-mazeCellPos.x, mazeCellPos.y), Quaternion.identity);
-        Instantiate(prefabInst, new Vector2(mazeCellPos.x, -mazeCellPos.y), Quaternion.identity);
-        Instantiate(prefabInst, new Vector2(-mazeCellPos.x, -mazeCellPos.y), Quaternion.identity);
+        
         
         //some useful code, please don't delete it
         // for (int i = 0; i < randomNumberOfCells; i++)
